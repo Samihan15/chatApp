d6beccc233f2db0f64c7e8631ed483be66c7fa35
Samihan <samihannandedkar15@gmail.com>
2023-12-25 18:05:48 +0530

    chat app socket file updated

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+
+// Removed Socket.IO initialization from here
 
 const PORT = process.env.PORT || 3000;
 
-http.listen(PORT,()=>{
-    console.log('Listening on port ',PORT);
+http.listen(PORT, () => {
+    console.log('Listening on port ', PORT);
 });
 
+app.use(express.static(__dirname + `/public/`));
 
-app.use(express.static(__dirname+`/public/`))
-
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/public/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection',(socket)=>{
-    console.log('connected !');
-
-    socket.on('message',(msg)=>{
-        socket.broadcast.emit('message',msg);
-    })
-})
+// Netlify doesn't support WebSocket connections directly in the main server file
+// Moved WebSocket logic to a serverless function (socket.js)
